6c738e543f225089b9226e99851232516242986b
LimYunHwi <88yhtserof@naver.com>
2022-09-17 20:56:00 +0900

    [FEAT] TabBar에 Contents 화면 추가 #4

---
+++ YaGi/YaGi/Scenes/ContentsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ContentsViewController.swift
+//  YaGi
+//
+//  Created by 임윤휘 on 2022/09/17.
+//
+
+import UIKit
+
+class ContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
