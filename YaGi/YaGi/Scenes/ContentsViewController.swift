071ec661e10a2d057a95f0cfb93365baab3e9752
LimYunHwi <88yhtserof@naver.com>
2022-09-22 20:25:40 +0900

    [FEAT] Contents 화면: 제목 버튼 구성 및 배치 #4

--- YaGi/YaGi/Scenes/ContentsViewController.swift
+++ YaGi/YaGi/Scenes/ContentsViewController.swift
@@ -17,12 +17,45 @@ class ContentsViewController: UIViewController {
         
         return item
     }()
+    
+    private lazy var titleButton: UIButton = {
+        var button = UIButton()
+        var configuration = UIButton.Configuration.plain()
+        
+        var attributedTitle = AttributedString("달러구트 꿈 백화점 1 해리를 찾아라")
+        attributedTitle.font = .maruburi(ofSize: 25, weight: .bold)
+        attributedTitle.foregroundColor = UIColor.yagiGrayDeep
+        
+        let paragraphTitle = NSMutableParagraphStyle()
+        paragraphTitle.lineSpacing = 0.3 * (attributedTitle.font?.lineHeight ?? 1)
+        paragraphTitle.lineBreakStrategy = .hangulWordPriority
+        
+        attributedTitle.paragraphStyle = paragraphTitle
+        
+        configuration.attributedTitle = attributedTitle
+        configuration.image = UIImage(systemName: "plus")
+        configuration.imagePadding = 10
+        configuration.imagePlacement = .trailing
+        configuration.imageColorTransformer = .init({_ in return UIKit.UIColor.yagiGrayLight})
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 50, leading: 0, bottom: 50, trailing: 0)
+        
+        let action = UIAction { action  in
+            //Present Write View
+            print("Present Write View")
+        }
+        
+        button.configuration = configuration
+        button.addAction(action, for: .touchUpInside)
+        
+        return button
+    }()
 
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationBar()
+        configureView()
     }
 }
 
@@ -30,5 +63,15 @@ class ContentsViewController: UIViewController {
 private extension ContentsViewController {
     func configureNavigationBar() {
         self.navigationItem.rightBarButtonItem = menuBarItem
+    }
+    
+    func configureView() {
+        [titleButton].forEach{ self.view.addSubview($0) }
+        
+        titleButton.snp.makeConstraints{ make in
+            make.top.equalToSuperview().inset(80)
+            make.leading.equalToSuperview().inset(20)
+            make.width.equalTo(300)
+        }
     }
 }
