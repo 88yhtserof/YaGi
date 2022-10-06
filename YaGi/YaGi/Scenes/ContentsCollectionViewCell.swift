52a9f7db975f2cb7816cdbef87fac91d8fa574f1
LimYunHwi <88yhtserof@naver.com>
2022-10-06 14:23:45 +0900

    [FEAT] Contents 화면: Contents 목록 cell 구성 #4

---
+++ YaGi/YaGi/Scenes/ContentsCollectionViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  ContentsCollectionViewCell.swift
+//  YaGi
+//
+//  Created by 임윤휘 on 2022/10/06.
+//
+
+import UIKit
+
+class ContentsCollectionViewCell: UICollectionViewCell {
+    private lazy var contentTitle: UILabel = {
+        let label = UILabel()
+        
+        label.text = "1. 예약하신 용이 도착하였습니다"
+        label.numberOfLines = 1
+        label.textColor = .yagiGrayDeep
+        label.font = .maruburi(ofSize: 22, weight: .semiBold)
+        label.textAlignment = .left
+        
+        return label
+    }()
+    
+    private lazy var contentDate: UILabel = {
+       let label = UILabel()
+        
+        label.text = "2022.10.06 목"
+        label.numberOfLines = 1
+        label.textColor = .yagiGray
+        label.font = .maruburi(ofSize: 18, weight: .regular)
+        label.textAlignment = .left
+        
+        return label
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureCell()
+    }
+    
+    func configureCell() {
+        [contentTitle, contentDate].forEach { self.contentView.addSubview($0) }
+        
+        contentTitle.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalToSuperview().inset(10)
+        }
+        
+        contentDate.snp.makeConstraints { make in
+            make.leading.equalTo(contentTitle.snp.leading)
+            make.trailing.equalTo(contentTitle.snp.trailing)
+            make.top.equalTo(contentTitle.snp.bottom).inset(10)
+            make.bottom.equalToSuperview().inset(10)
+        }
+    }
+}
