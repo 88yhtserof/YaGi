bcfeb1f5c2316f45dcb474bdc10f9f575fa9953d
LimYunHwi <88yhtserof@naver.com>
2022-10-23 17:46:11 +0900

    [FEAT] Detail: Content 배치 #9

--- YaGi/YaGi/Scenes/ContentDetailViewController.swift
+++ YaGi/YaGi/Scenes/ContentDetailViewController.swift
@@ -28,12 +28,28 @@ class ContentDetailViewController: UIViewController {
         return label
     }()
     
-    private lazy var colorView: UIView = {
+    private lazy var contentView: UIView = {
         let view = UIView()
         
-        view.backgroundColor = .yagiWhihtDeep
-        
         return view
+    }()
+    
+    private lazy var contentTextView: UITextView = {
+        var textView = UITextView()
+        
+        var testText = ""
+        for i in 1...10 {
+            testText.append(contentsOf: """
+                            상강은 한로(寒露)와 입동(立冬) 사이에 들며, 태양의 황경이 210도에 이를 때로 양력으로 10월 23일 무렵이 된다. 이 시기는 가을의 쾌청한 날씨가 계속되는 대신에 밤의 기온이 매우 낮아지는 때이다. 따라서 수증기가 지표에서 엉겨 서리가 내리며, 온도가 더 낮아지면 첫 얼음이 얼기도 한다.
+                            """)
+        }
+        
+        textView.text = testText
+        textView.isScrollEnabled = false
+        textView.textColor = .yagiGrayDeep
+        textView.font = .maruburi(ofSize: 20, weight: .regular)
+        
+        return textView
     }()
     
     private lazy var menuBarItem: UIBarButtonItem = {
@@ -79,7 +95,8 @@ private extension ContentDetailViewController {
         self.view.backgroundColor = .yagiWhite
         
         [scrollView].forEach { self.view.addSubview($0) }
-        [contentTitle, colorView].forEach { scrollView.addSubview($0) }
+        [contentTitle, contentView].forEach { scrollView.addSubview($0) }
+        [contentTextView].forEach { contentView.addSubview($0) }
         
         scrollView.snp.makeConstraints { make in
             make.top.leading.trailing.bottom.equalToSuperview()
@@ -91,11 +108,16 @@ private extension ContentDetailViewController {
             make.width.equalTo(300)
         }
         
-        colorView.snp.makeConstraints { make in
+        contentView.snp.makeConstraints { make in
             make.top.equalTo(contentTitle.snp.bottom).offset(40)
-            make.leading.trailing.bottom.equalToSuperview().inset(30)
-            make.height.equalTo(3000)
-            make.width.equalTo(100)
+            make.bottom.equalToSuperview().inset(40)
+            make.leading.trailing.equalToSuperview()
+            make.width.equalTo(scrollView.snp.width)
+        }
+        
+        contentTextView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(30)
         }
     }
 }
