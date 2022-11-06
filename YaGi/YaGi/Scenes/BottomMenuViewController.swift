d78f54bebb314c096bd8eccb408b04f84b2a7081
LimYunHwi <88yhtserof@naver.com>
2022-11-06 16:54:23 +0900

    [FEAT] BottomMenu: LifeCycle 설정 #11

--- YaGi/YaGi/Scenes/BottomMenuViewController.swift
+++ YaGi/YaGi/Scenes/BottomMenuViewController.swift
@@ -130,6 +130,11 @@ class BottomMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
         configureView()
         configurePresent()
     }
