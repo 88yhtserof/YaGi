38d700547687bb46485b2196fde8c4e679cffabe
LimYunhwi <88yhtserof@naver.com>
2023-01-05 13:27:09 +0900

    [FEAT] Writing: '취소' 버튼 구현 #17

--- YaGi/YaGi/Scenes/WritingViewController.swift
+++ YaGi/YaGi/Scenes/WritingViewController.swift
@@ -6,13 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class WritingViewController: UIViewController {
-
+    private lazy var cancelButton: UIButton = {
+        var button = UIButton()
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(systemName: "multiply")
+        configuration.baseForegroundColor = .yagiGray
+        
+        let action = UIAction { _ in
+            self.dismiss(animated: true)
+        }
+        
+        button.configuration = configuration
+        button.addAction(action, for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureView()
+        
+    }
+}
+
+private extension WritingViewController {
+    
+    func configureView() {
         self.view.backgroundColor = .yagiWhite
         
+        [
+            cancelButton
+        ].forEach { view.addSubview($0) }
+        
+        cancelButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.centerX.equalToSuperview()
+        }
     }
 }
