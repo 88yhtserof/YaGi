b486bfce3fef4d1e9e90e4bc05335bd49910e9bb
LimYunhwi <88yhtserof@naver.com>
2023-01-05 16:52:49 +0900

    [FEAT] Writing: Scroll 및 TextView 구현 #17

--- YaGi/YaGi/Scenes/WritingViewController.swift
+++ YaGi/YaGi/Scenes/WritingViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 import SnapKit
 
 class WritingViewController: UIViewController {
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        scrollView.delegate = self
+        
+        return scrollView
+    }()
+    
     private lazy var cancelButton: UIButton = {
         var button = UIButton()
         var configuration = UIButton.Configuration.plain()
@@ -38,6 +47,33 @@ class WritingViewController: UIViewController {
         
     }()
     
+    private lazy var writingView: UITextView = {
+        let textView = UITextView()
+        
+        var testText = ""
+        for i in 1...10 {
+            testText.append(contentsOf: """
+                            상강은 한로(寒露)와 입동(立冬) 사이에 들며, 태양의 황경이 210도에 이를 때로 양력으로 10월 23일 무렵이 된다. 이 시기는 가을의 쾌청한 날씨가 계속되는 대신에 밤의 기온이 매우 낮아지는 때이다. 따라서 수증기가 지표에서 엉겨 서리가 내리며, 온도가 더 낮아지면 첫 얼음이 얼기도 한다.
+                            """)
+        }
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 14
+        paragraphStyle.lineBreakStrategy = .hangulWordPriority
+        
+        let attributes: [NSAttributedString.Key : Any] = [NSAttributedString.Key.paragraphStyle : paragraphStyle]
+        let attributedText = NSAttributedString(string:testText, attributes: attributes)
+        
+        textView.attributedText = attributedText
+        textView.isEditable = true
+        textView.text = testText
+        textView.isScrollEnabled = false
+        textView.textColor = .yagiGrayDeep
+        textView.font = .maruburi(ofSize: 20, weight: .regular)
+        
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -51,20 +87,40 @@ private extension WritingViewController {
     func configureView() {
         self.view.backgroundColor = .yagiWhite
         
+        view.addSubview(scrollView)
+        
         [
             cancelButton,
-            datePicker
+            datePicker,
+            writingView
         ]
-            .forEach { view.addSubview($0) }
+            .forEach { scrollView.addSubview($0) }
+        
+        let inset: Int = 25
+        let offset: Int = 20
+        
+        scrollView.snp.makeConstraints { make in
+            make.verticalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(inset)
+        }
         
         cancelButton.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.top.equalToSuperview().inset(inset)
             make.centerX.equalToSuperview()
         }
         
         datePicker.snp.makeConstraints { make in
-            make.top.equalTo(cancelButton.snp.bottom).offset(20)
-            make.leading.equalToSuperview().inset(25)
+            make.top.equalTo(cancelButton.snp.bottom).offset(offset)
+            make.leading.equalToSuperview()
+        }
+        
+        writingView.snp.makeConstraints { make in
+            make.top.equalTo(datePicker.snp.bottom).offset(offset)
+            make.bottom.equalToSuperview().inset(inset)
+            make.horizontalEdges.equalToSuperview()
+            make.width.equalTo(scrollView.snp.width)
         }
     }
 }
+
+extension WritingViewController: UIScrollViewDelegate {}
