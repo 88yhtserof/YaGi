5cad52704aeef2b3bcfc89529e307ccfb0983657
LimYunhwi <88yhtserof@naver.com>
2023-01-05 12:37:27 +0900

    [DEL]: 불필요한 주석 삭제 #15

--- YaGi/YaGi/Scenes/ShareActivityItemSource.swift
+++ YaGi/YaGi/Scenes/ShareActivityItemSource.swift
@@ -21,34 +21,28 @@ class ShareActivityItemSource:  NSObject, UIActivityItemSource {
     }
     
     //Returns the placeholder object for the data.
-    //데이터에 대한 미리보기 반환
     func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
         return placeholder
     }
     
     //Returns the data object to be acted upon.
-    //행동 될 데이터 객체 반환
     func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
         return content
     }
     
     //For activities that support a subject field, returns the subject for the item.
-    //제목 필드를 지원하는 액티비티에 대해, 아이템에 대한 제목을 반환
     func activityViewController(_ activityViewController: UIActivityViewController, subjectForActivityType activityType: UIActivity.ActivityType?) -> String {
         return title
     }
 
     //Returns metadata to display in the preview header of the share sheet.
-    //share sheet의 헤더에 미리보기로 보여줄 메타데이터를 반환한다.
     func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
         let metadata = LPLinkMetadata()
         metadata.title = title
         // TODO: - Change to AppIcon
         metadata.iconProvider = NSItemProvider(object: UIImage(systemName: "text.bubble")!)
-        //This is a bit ugly, though I could not find other ways to show text content below title.
-        //https://stackoverflow.com/questions/60563773/ios-13-share-sheet-changing-subtitle-item-description
-        //You may need to escape some special characters like "/".
         metadata.originalURL = URL(fileURLWithPath: placeholder )
+        
         return metadata
     }
 }
