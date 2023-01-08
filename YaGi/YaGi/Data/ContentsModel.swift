cbc233af0d4e4699a5f8e207caa9b28442ba2cfa
LimYunhwi <88yhtserof@naver.com>
2023-01-08 13:45:05 +0900

    [ADD] ContentsModel 생성 #19

---
+++ YaGi/YaGi/Data/ContentsModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentsModel.swift
+//  YaGi
+//
+//  Created by 임윤휘 on 2023/01/08.
+//
+
+import Foundation
+
+struct ContentsModel {
+    let title: String
+    let contents: [ContentModel]
+}
