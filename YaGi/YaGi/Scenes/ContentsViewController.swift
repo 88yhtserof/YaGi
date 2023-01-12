4a0239173a352a5cda4cab00f379d2584b0edb78
LimYunhwi <88yhtserof@naver.com>
2023-01-12 18:12:23 +0900

    [FEAT] Content: 책 제목 및 새로운 글 작성 버튼 UI 수정 #19

--- YaGi/YaGi/Scenes/ContentsViewController.swift
+++ YaGi/YaGi/Scenes/ContentsViewController.swift
@@ -29,26 +29,42 @@ class ContentsViewController: UIViewController {
         return item
     }()
     
+    private lazy var titleLabel: UILabel = {
+        var label = UILabel()
+        
+        let paragraphTitle = NSMutableParagraphStyle()
+        paragraphTitle.lineSpacing = 10
+        paragraphTitle.lineBreakStrategy = .hangulWordPriority
+        
+        let attributes = [NSAttributedString.Key.paragraphStyle : paragraphTitle]
+        let attributeTitle = NSAttributedString(string: self.bookTitle, attributes: attributes)
+        
+        label.attributedText = attributeTitle
+        label.font = .maruburi(ofSize: 35, weight: .bold)
+        label.textColor = UIColor.yagiGrayDeep
+        label.numberOfLines = 2
+        label.lineBreakMode = NSLineBreakMode.byTruncatingTail
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        
+        return label
+    }()
+    
     private lazy var titleButton: UIButton = {
         var button = UIButton()
         var configuration = UIButton.Configuration.plain()
-        
-        var attributedTitle = AttributedString(self.bookTitle)
-        attributedTitle.font = .maruburi(ofSize: 25, weight: .bold)
-        attributedTitle.foregroundColor = UIColor.yagiGrayDeep
-        
-        let paragraphTitle = NSMutableParagraphStyle()
-        paragraphTitle.lineSpacing = 0.3 * (attributedTitle.font?.lineHeight ?? 1)
-        paragraphTitle.lineBreakStrategy = .hangulWordPriority
-        
-        attributedTitle.paragraphStyle = paragraphTitle
-        
+
+        var attributedTitle = AttributedString("이야기 추가하기")
+        attributedTitle.font = .maruburi(ofSize: 15, weight: .regular)
+        attributedTitle.foregroundColor = UIColor.yagiHighlight
+
         configuration.attributedTitle = attributedTitle
-        configuration.image = UIImage(systemName: "plus")
-        configuration.imagePadding = 10
+        configuration.image = UIImage(systemName: "text.badge.plus")
+        configuration.imagePadding = 3
         configuration.imagePlacement = .trailing
-        configuration.imageColorTransformer = .init({_ in return UIKit.UIColor.yagiGrayLight})
-        configuration.contentInsets = NSDirectionalEdgeInsets(top: 50, leading: 0, bottom: 50, trailing: 0)
+        configuration.buttonSize = .mini
+        configuration.imageColorTransformer = .init({_ in return UIKit.UIColor.yagiHighlight})
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 50)
         
         let action = UIAction { action  in
             let writingViewController = WritingViewController()
@@ -109,16 +125,26 @@ private extension ContentsViewController {
     }
     
     func configureView() {
-        [titleButton, contentsCollectionView].forEach{ self.view.addSubview($0) }
+        [
+            titleLabel,
+            titleButton,
+            contentsCollectionView
+        ]
+            .forEach{ self.view.addSubview($0) }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.leading.equalToSuperview().inset(25)
+            make.trailing.equalToSuperview().inset(50)
+        }
         
         titleButton.snp.makeConstraints{ make in
-            make.top.equalToSuperview().inset(80)
-            make.leading.equalToSuperview().inset(20)
-            make.width.equalTo(300)
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview().inset(25)
         }
         
         contentsCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(titleButton.snp.bottom)
+            make.top.equalTo(titleButton.snp.bottom).offset(15)
             make.leading.trailing.equalToSuperview().inset(15)
             make.bottom.equalToSuperview().inset(100)
         }
