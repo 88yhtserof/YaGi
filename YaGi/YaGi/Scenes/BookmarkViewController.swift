19a8304edadee46f85e58696aed663cc54832d9d
LimYunhwi <88yhtserof@naver.com>
2023-01-25 14:13:45 +0900

    [CORRECT] BookmarkViewController 타입 변경 #23

--- YaGi/YaGi/Scenes/BookmarkViewController.swift
+++ YaGi/YaGi/Scenes/BookmarkViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BookmarkViewController: UITableViewController {
+class BookmarkViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
