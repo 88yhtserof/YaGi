bcaba2d02265910b9568b598d94d4760e1fa07de
LimYunhwi <88yhtserof@naver.com>
2023-01-31 21:14:32 +0900

    [FEAT] Setting: 책 정보 UI 수정 #27

--- YaGi/YaGi/Scenes/SettingViewController.swift
+++ YaGi/YaGi/Scenes/SettingViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 class SettingViewController: UIViewController {
     let book = UserDefaultsManager.books?.first
     
+    private lazy var lineView: UIView = {
+        let view = UIView()
+        
+        view.backgroundColor = .yagiHighlight
+        view.layer.shadowColor = UIColor.yagiWhihtDeep.cgColor
+        view.alpha = 0.5
+        
+        view.layer.shadowColor = UIColor.yagiWhihtDeep.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: 2)
+        view.layer.shadowOpacity = 1
+        
+        return view
+    }()
+    
     private lazy var bookInfoContainerStackView: UIStackView = {
         let stackView = UIStackView()
         
         stackView.distribution = .equalSpacing
-        stackView.alignment = .leading
+        stackView.alignment = .center
         stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.layoutMargins = UIEdgeInsets(top: 30, left: 25, bottom: 30, right: 25)
+        stackView.spacing = 15
+        
+        stackView.layoutMargins = UIEdgeInsets(top: 25, left: 25, bottom: 45, right: 25)
         stackView.isLayoutMarginsRelativeArrangement = true
-        stackView.layer.borderWidth = 2
-        stackView.layer.borderColor = UIColor.yagiWhihtDeep.cgColor
-        stackView.roundCorner(round: 15, [ .topRight, .topLeft, .bottomLeft, .bottomRight ])
         
         return stackView
     }()
@@ -33,9 +45,10 @@ class SettingViewController: UIViewController {
         let text = book.title
         
         label.text = text
+        label.textAlignment = .center
         label.textColor = .yagiGrayDeep
-        label.font = .maruburi(ofSize: 25, weight: .semiBold)
-        label.numberOfLines = 3
+        label.font = .maruburi(ofSize: 25, weight: .bold)
+        label.numberOfLines = 4
         label.lineBreakStrategy = .hangulWordPriority
         
         return label
@@ -45,8 +58,8 @@ class SettingViewController: UIViewController {
         let stackView = UIStackView()
         
         stackView.axis = .horizontal
-        stackView.spacing = 15
-        stackView.alignment = .leading
+        stackView.spacing = 10
+        stackView.alignment = .center
         stackView.distribution = .fill
         
         return stackView
@@ -58,7 +71,7 @@ class SettingViewController: UIViewController {
         guard let book = self.book else { return UILabel() }
         label.text = book.date
         label.textColor = .yagiGrayDeep
-        label.font = .maruburi(ofSize: 15, weight: .light)
+        label.font = .maruburi(ofSize: 15, weight: .semiBold)
         
         return label
     }()
@@ -69,14 +82,14 @@ class SettingViewController: UIViewController {
         guard let contents = self.book?.contents else { return UILabel() }
         label.text = contents.count.description + "장"
         label.textColor = .yagiGrayDeep
-        label.font = .maruburi(ofSize: 15, weight: .light)
+        label.font = .maruburi(ofSize: 15, weight: .semiBold)
         
         return label
     }()
     
     private lazy var sendEmailButton: UIButton = {
         let attributes = AttributeContainer(
-            [NSAttributedString.Key.font : UIFont(name: "MaruBuri-SemiBold", size: 20) ?? UIFont()]
+            [NSAttributedString.Key.font : UIFont(name: "MaruBuri-Regular", size: 20) ?? UIFont()]
         )
         let attribitedTitle = AttributedString("문의하기", attributes: attributes)
         var configuration = UIButton.Configuration.plain()
@@ -91,7 +104,7 @@ class SettingViewController: UIViewController {
     
     private lazy var appVersionButton: UIButton = {
         let attributes = AttributeContainer(
-            [NSAttributedString.Key.font : UIFont(name: "MaruBuri-SemiBold", size: 20) ?? UIFont()]
+            [NSAttributedString.Key.font : UIFont(name: "MaruBuri-Regular", size: 20) ?? UIFont()]
         )
         let attribitedTitle = AttributedString("앱 버전 0.0.1", attributes: attributes)
         var configuration = UIButton.Configuration.plain()
@@ -146,18 +159,25 @@ private extension SettingViewController {
         
         [
             bookInfoContainerStackView,
+            lineView,
             settingButtonStackView
         ]
             .forEach{ view.addSubview($0) }
         
         bookInfoContainerStackView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.top.equalTo(view.safeAreaLayoutGuide)
             make.leading.trailing.equalToSuperview().inset(20)
         }
         
+        lineView.snp.makeConstraints{ make in
+            make.top.equalTo(bookInfoContainerStackView.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(1.5)
+        }
+        
         settingButtonStackView.snp.makeConstraints{ make in
-            make.top.equalTo(bookInfoContainerStackView.snp.bottom).offset(40)
-            make.leading.equalTo(bookInfoContainerStackView)
+            make.top.equalTo(lineView.snp.bottom).offset(40)
+            make.leading.equalToSuperview().inset(20)
         }
     }
 }
