7137aa41b5c1fe5ef9d7094cef64322ddf11dcd1
LimYunhwi <88yhtserof@naver.com>
2023-02-20 14:22:35 +0900

    [FEAT] Bookshelf: 책 제목 및 책 표지 디자인 이미지 UI qocl #52

--- YaGi/YaGi/Scenes/BookshelfViewController.swift
+++ YaGi/YaGi/Scenes/BookshelfViewController.swift
@@ -18,6 +18,27 @@ class BookshelfViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var bookTitleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "책 제목 Label"
+        label.font = .maruburi(ofSize: 20, weight: .semiBold)
+        label.textColor = .yagiGrayDeep
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private lazy var bookcoverDesignImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        // TODO: - 이미지 위치 확인용이므로 수정 필요
+        imageView.backgroundColor = .yagiGrayDeep
+        
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,12 +51,31 @@ private extension BookshelfViewController {
         self.view.backgroundColor = .yagiWhite
         
         [
+            bookcoverDesignImageView,
+            bookTitleLabel
+        ]
+            .forEach{ bookcoverImageView.addSubview($0) }
+        
+        [
             bookcoverImageView
         ]
             .forEach{ view.addSubview($0) }
         
         bookcoverImageView.snp.makeConstraints{ make in
             make.center.equalToSuperview()
+        }
+        
+        bookTitleLabel.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(200)
+            make.top.equalToSuperview().inset(100)
+        }
+        
+        bookcoverDesignImageView.snp.makeConstraints{ make in
+            make.leading.trailing.equalToSuperview().inset(26)
+            make.bottom.equalToSuperview().inset(3)
+            make.top.equalTo(bookTitleLabel.snp.bottom)
+            
         }
     }
 }
