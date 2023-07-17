20bbe93cf094342d8a4d33a024a27bcbe0a1ff12
88yhtserof <88yhtserofg@gmail.com>
2023-07-17 13:51:28 +0900

    [ADD] UserDefaultsManager: layout 변수 추가 #73

--- YaGi/YaGi/Data/UserDafaultsManager.swift
+++ YaGi/YaGi/Data/UserDafaultsManager.swift
@@ -11,6 +11,9 @@ struct UserDefaultsManager {
     @UserDefaultWrapper(dictionaryKey: "Books", defaultValue: nil)
     static var books: [BookModel]?
     
+    @UserDefaultWrapper(dictionaryKey: "Layout", defaultValue: SettingLayoutModel())
+    static var layout: SettingLayoutModel?
+    
     @UserDefaultStandardWrapper(dictionaryKey: "isImplovedData", defaultValue: false)
     static var isImplovedData: Bool?
 }
